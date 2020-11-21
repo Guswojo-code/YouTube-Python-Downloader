06865eba7a90664ca57e4fbc46809914478a1aba
Guswojo-code <74015971+Guswojo-code@users.noreply.github.com>
2020-11-21 15:40:48 +0100

    Add files via upload

---
+++ YouTubeDownloader.py
@@ -0,0 +1,9 @@
+from pytube import YouTube 
+#Before this will work you need to go to commandprompt and write pip install pytube
+
+Link = input("Your youtube link: ")
+
+yt = (Link)
+
+w = YouTube(yt).streams.first()
+w.download(output_path="Choose your folder")
