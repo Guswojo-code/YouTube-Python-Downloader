1a06a010ec4cb77ca383d0613305ddacfc8574a5
Guswojo-code <74015971+Guswojo-code@users.noreply.github.com>
2021-05-26 11:55:13 +0200

    Output Commentary

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -6,5 +6,5 @@ Link = input("Your youtube link: ")
 yt = (Link)
 
 w = YouTube(yt).streams.first()
-w.download(output_path="Choose your folder")
+w.download(output_path="Choose your folder") #Output folder
 print("Made one new file from " + yt)
